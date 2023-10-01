f25266f0ba34e6fe8fb8186b92d07d9277000010
Newberry <146621059+beckerryann@users.noreply.github.com>
2023-10-01 15:56:48 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,149 @@
+![bfx-cpp-api logo](app/doc/logo/bfx-cpp-api_logo.png)
+
+***
+
+### Build status
+
+| [Linux][lin-link] |
+| :---------------: |
+| ![lin-badge]      |
+
+[lin-badge]: https://circleci.com/gh/MMquant/bfx-cpp-api/tree/master.svg?style=svg "CircleCI build status"
+[lin-link]:  https://circleci.com/gh/MMquant/bfx-cpp-api "CircleCI build status"
+
+### Notice
+
+***Master*** branch contains new version of the client. For old version checkout ***legacy*** branch.
+
+### Synopsis
+
+This header-only library contains class for interfacing Bitfinex REST API v1. Current version supports response JSON
+schema validation.
+
+### Dependencies
+
+*bfx-cpp-api* depends on following external libraries/packages
+
+* *cmake* - [https://cmake.org/download/](https://cmake.org/download/)
+* *libcrypto++* - [https://www.cryptopp.com/](https://www.cryptopp.com/)
+* *libcrypto++-dev* - [https://www.cryptopp.com/](https://www.cryptopp.com/)
+* *curl* - [https://curl.haxx.se/download.html](https://curl.haxx.se/download.html)
+* *libcurl4-gnutls-dev* or *libcurl4-openssl-dev* - [https://curl.haxx.se/download.html](https://curl.haxx.se/download.html)
+* *gcc* > 7.2 or C++14 compatible *clang*
+
+### How to Build'n'Run `src/example.cpp`
+
+1. Install dependencies (via apt, homebrew etc.).
+2. Clone or download *bfx-api-cpp* repository.
+3. Add `key-secret` file in `bfx-api-cpp/app/doc` directory. (or edit `example.cpp` so that it doesn't use `key-secret` file)
+4. Peek into self-documented `<your_project_dir>/app/src/example.cpp`.
+5. Build `example` binary
+
+```BASH
+cd <your_project_dir>app/build && cmake .. && make
+```
+
+7. Run `example` binary from `<your_project_dir>app/bin`
+
+```BASH
+./example
+```
+
+### How to Build'n'Run `src/example.cpp` in Docker container
+
+1. Clone or download *bfx-api-cpp* repository.
+2. Build docker image
+
+```BASH
+cd <your_project_dir>
+docker-compose build
+```
+
+3. Start docker image
+
+```BASH
+docker-compose up &
+```
+
+4. Spawn bash
+
+```BASH
+docker exec -it bfx-cpp-api_dev_1 /bin/sh
+```
+
+5. Add `key-secret` file in `/home/bfx-cpp-api/app/doc` directory. (or edit `example.cpp` so that it doesn't use `key-secret` file)
+
+```BASH
+cd /home/bfx-cpp-api/app/doc
+echo <key> > key-secret
+echo <secret> >> key-secret
+```
+
+6. Build example
+
+```BASH
+cd /home/bfx-cpp-api/app/build
+cmake ..
+make
+```
+
+7. Run `example` binary
+
+```BASH
+cd /home/bfx-cpp-api/app/bin
+./example
+```
+
+### Quick interface overview
+
+```C++
+// Create API client for both authenticated and unauthenticated requests
+BfxAPI::BitfinexAPI bfxAPI("accessKey", "secretKey");
+
+// Create API client for just unauthenticated requests
+BfxAPI::BitfinexAPI bfxAPI();
+
+// Fetch data
+bfxAPI.getTicker("btcusd");
+
+// Check for errors
+if (!bfxAPI.hasApiError())
+{
+    // Get response in string
+    cout << bfxAPI.strResponse() << endl;
+}
+else
+{
+    // Inspect errors
+    cout << bfxAPI.getBfxApiStatusCode() << endl;
+    cout << bfxAPI.getCurlStatusCode() << endl;
+}
+```
+
+See self-explanatory `src/example.cpp` for general usage and more requests.
+
+### Change Log
+
+- 2018-09-26 Using the small Docker image Alpine instead of Debian.
+- 2018-09-26 Using docker-compose to build/up/down the image.
+- 2018-09-26 Grouping project files inside the app folder.
+- 2018-08-01 Dockerfile added. CircleCI added.
+- 2018-07-24 CMakeLists.txt added. Installation instructions changed.
+- 2018-07-11 Schema validation logic complete. Client currently validates public requests only.
+
+### Known issues
+
+You will not be able to compile *bfx-cpp-api* with GCC<7.2 due to this [bug](https://gcc.gnu.org/bugzilla/show_bug.cgi?id=66297).
+You can use CLANG to avoid GCC bug.
+
+### Contribution
+
+1. Fork `bfx-cpp-api` repository.
+2. Commit to your ***develop*** branch.
+3. Create pull request from your ***develop*** branch to ***origin/develop*** branch.
+
+No direct pull request to ***master*** branch accepted!
+
+### Author
+
+Petr Javorik [www.mmquant.net](www.mmquant.net) [maple@mmquant.net](maple@mmquant.net)
---
+++ app/src/example.cpp
@@ -0,0 +1,158 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  example.cpp
+//
+//
+//  Bitfinex REST API C++ client - examples
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// std
+#include <fstream>
+#include <iostream>
+
+// BitfinexAPI
+#include "bfx-api-cpp/BitfinexAPI.hpp"
+
+
+// namespaces
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+
+
+int main(int argc, char *argv[])
+{
+    // Create bfxAPI without API keys
+    BfxAPI::BitfinexAPI bfxAPI;
+
+    // Create bfxAPI with API keys
+    // BfxAPI::BitfinexAPI bfxAPI("accessKey", "secretKey");
+
+    // Load API keys from file
+    ifstream ifs("../doc/key-secret", ifstream::in);
+    if (ifs.is_open()) {
+        string accessKey; getline(ifs, accessKey);
+        string secretKey; getline(ifs, secretKey);
+        ifs.close();
+        bfxAPI.setKeys(accessKey, secretKey);
+    }
+
+    // Fetch API
+    cout << "Request with error checking: " << endl;
+    bfxAPI.getTicker("btcusd");
+    if (!bfxAPI.hasApiError())
+        cerr << bfxAPI.strResponse() << endl;
+    else
+    {
+        // see bfxERR enum in BitfinexAPI.hpp::BitfinexAPI
+        cerr << "BfxApiStatusCode: ";
+        cerr << bfxAPI.getBfxApiStatusCode() << endl;
+        // see https://curl.haxx.se/libcurl/c/libcurl-errors.html
+        cerr << "CurlStatusCode: ";
+        cerr << bfxAPI.getCurlStatusCode() << endl;
+    }
+
+    cout << "Request without error checking: " << endl;
+    cout << bfxAPI.getSummary().strResponse() << endl;
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///  Available unauthenticated requests
+    ////////////////////////////////////////////////////////////////////////////
+
+    //  bfxAPI.getTicker("btcusd");
+    //  bfxAPI.getStats("btcusd");
+    //  bfxAPI.getFundingBook("USD", 50, 50);
+    //  bfxAPI.getOrderBook("btcusd", 50, 50, true);
+    //  bfxAPI.getTrades("btcusd", 0L, 50);
+    //  bfxAPI.getLends("USD", 0L, 50);
+    //  bfxAPI.getSymbols();
+    //  bfxAPI.getSymbolsDetails();
+
+    ////////////////////////////////////////////////////////////////////////////
+    ///  Available authenticated requests
+    ////////////////////////////////////////////////////////////////////////////
+
+    ///  Account  ///
+    //  bfxAPI.getAccountInfo();
+    //  bfxAPI.getAccountFees();
+    //  bfxAPI.getSummary();
+    //  bfxAPI.deposit("bitcoin", "deposit", true);
+    //  bfxAPI.getKeyPermissions();
+    //  bfxAPI.getMarginInfos();
+    //  bfxAPI.getBalances();
+    //  bfxAPI.transfer(0.1, "BTC", "trading", "deposit");
+    //  bfxAPI.withdraw(); // configure withdraw.conf file before use
+
+    ///  Orders  ///
+    //  bfxAPI.newOrder("btcusd",
+    //                  0.01,
+    //                  983,
+    //                  "sell",
+    //                  "exchange limit",
+    //                  false,
+    //                  true,
+    //                  false,
+    //                  false,
+    //                  0);
+    //
+    //  How to create vOrders object for newOrders() call
+    //  BitfinexAPI::vOrders orders =
+    //  {
+    //      {"btcusd", 0.1, 950, "sell", "exchange limit"},
+    //      {"btcusd", 0.1, 950, "sell", "exchange limit"},
+    //      {"btcusd", 0.1, 950, "sell", "exchange limit"}
+    //  };
+    //  bfxAPI.newOrders(orders);
+    //
+    //  bfxAPI.cancelOrder(13265453586LL);
+    //
+    //  How to create ids object for cancelOrders() call
+    //  BitfinexAPI::vIds ids =
+    //  {
+    //      12324589754LL,
+    //      12356754322LL,
+    //      12354996754LL
+    //  };
+    //  bfxAPI.cancelOrders(ids);
+    //
+    //  bfxAPI.cancelAllOrders();
+    //  bfxAPI.replaceOrder(1321548521LL,
+    //                      "btcusd",
+    //                      0.05,
+    //                      1212,
+    //                      "sell",
+    //                      "exchange limit",
+    //                      false,
+    //                      false);
+    //  bfxAPI.getOrderStatus(12113548453LL);
+    //  bfxAPI.getActiveOrders();
+    //  bfxAPI.getOrdersHistory(10);
+
+    ///  Positions  ///
+    //  bfxAPI.getActivePositions();
+    //  bfxAPI.claimPosition(156321412LL, 150);
+
+    ///  Historical data  ///
+    //  bfxAPI.getBalanceHistory("USD", 0L, 0L, 500, "all");
+    //  bfxAPI.getWithdrawalHistory("BTC", "all", 0L , 0L, 500);
+    //  bfxAPI.getPastTrades("btcusd", 0L, 0L, 500, false);
+
+    ///  Margin funding  ///
+    //  bfxAPI.newOffer("USD", 12000, 25.2, 30, "lend");
+    //  bfxAPI.cancelOffer(12354245628LL);
+    //  bfxAPI.getOfferStatus(12313541215LL);
+    //  bfxAPI.getActiveCredits();
+    //  bfxAPI.getOffers();
+    //  bfxAPI.getOffersHistory(50);
+    //  bfxAPI.getPastFundingTrades("BTC", 0, 50);
+    //  bfxAPI.getTakenFunds();
+    //  bfxAPI.getUnusedTakenFunds();
+    //  bfxAPI.getTotalTakenFunds();
+    //  bfxAPI.closeLoan(1235845634LL);
+    //  bfxAPI.closePosition(1235845634LL);
+
+    return 0;
+}
